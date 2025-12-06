931826dbc2603bad35009fe39c90f941efb3053c
Farid Roziqov <faridroziqov68@gmail.com>
2025-12-06 10:11:09 +0500

    kife

--- app/components/firstchild.tsx
+++ app/components/firstchild.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import "./firstchild.scss";
-import Image from "next/image";
+// import Image from "next/image";
 
 const FirstChild = () => {
   return (
